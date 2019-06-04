42dbd6c224b1717992b63ca642eadfe2084db4f1
Robert1Kowalski <kowals.robert@gmail.com>
2019-06-03 23:07:26 -0600

    Deleted the Functions becasue they were all broken. Going to try an if/else statements with less functions involved.

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,38 +1,6 @@
 var letters = [ "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var randomLetter = "";
-var winCount = 0;
-var lossCount = 0;
-var guessesLeft = 0;
-var guessedLetter = [];
-var defaultGuessesLeft = 9;
-
-window.addEventListener("keypress", onKeyPress, false);
-game(); 
-function game(){
-    guessedLetter = [];
-    resetGuesses();
-    generateRandomLetter();
-    console.log(randomLetter) // just to check
-}
 
 
-function onKeyPress(){
- if (letters.includes(letters, 0) && guessedLetters.includes(letters,0) === false){
-     guessedLetter.push(letter);
-     printKeyPressed (letter);
-     guessesLeft --;
-     displayonScreen("guessesLeft")
- }
-}
-
-function generateRandomLetter(){
-    randomLetter = letters[Math.floor(Math.random()*letters.length)]
-}
-
-function resetGuesses (){
-    // guessesLeft = resetGuesses ??
-    
-}
 ///Still Need To Do:
 // build function for resetGuessses.  To reset the game I need to build a function that resets the page when defaultGuessesLeft = 0 
 //funciton resetGuesses (){
