58c4eb0b7f41f77499b5933656775aba2338a99e
Robert1Kowalski <kowals.robert@gmail.com>
2019-08-19 06:20:57 -0600

    fixed file error, app functions again

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,51 +1,83 @@
-var letters = [ "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var letters = ["a", "b", "c"];
 
-var win = 0;
+// This array will hold what we guess
+var guessedLetters = [];
+
+// This variable will be randomly assigned one of the three letters
+var letterToGuess = null;
+
+// This is what we'll use to count down
+var guessesLeft = 9;
+
+// This is the counter for wins/losses
+var wins = 0;
 var losses = 0;
-var GuessLeft = 9; // win and losses at 0 b/c it is the beginning of game
 
-document.onkeyup = function () { // one giant function
-                                // (1) IF the user input === the psychic guess, win total ++
-                                // (2)  if guessesLeft hits zero, add one to the loss column;
-                                // (3)  anything else subtracts one from guessesleft 
+// Below we created three functions to updateGuesses, updateGuessesLeft, and updateGuessesSoFar
+var updateGuessesLeft = function() {
+  // Here we are grabbing the HTML element and setting it equal to the guessesLeft.
+  // (i.e. guessesLeft will get displayed in HTML)
+  document.querySelector("#guesses-left").innerHTML = guessesLeft;
+};
+
+var updateLetterToGuess = function() {
+  // Here we get a random letterToGuess and assign it based on a random generator (only looking at a, b, or c)
+  letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+};
+
+var updateGuessesSoFar = function() {
+  // Here we take the guesses the user has tried -- then display it as letters separated by commas.
+  document.querySelector("#guesses-so-far").innerHTML = guessedLetters.join(", ");
+};
+
+// Function will be called when we reset everything
+var reset = function() {
+  guessesLeft = 9;
+  guessedLetters = [];
+  updateLetterToGuess();
+  updateGuessesLeft();
+  updateGuessesSoFar();
+};
+
+// Execute on page load.
+updateLetterToGuess();
+updateGuessesLeft();
+
+// This function will capture the keyboard clicks.
+document.onkeydown = function(event) {
+  // It's going to reduce the guesses by one
+  guessesLeft--;
+
+  // Lowercase the letter
+  var letter = event.key.toLowerCase();
+
+  // Then add the guess to the guessedLetters array
+  guessedLetters.push(letter);
+
+  // Then its going to run the update functions
+  updateGuessesLeft();
+  updateGuessesSoFar();
 
 
+  // We'll check if there's a match.
+  if (letter === letterToGuess) {
 
-// need to build varaibles to hold the psychic value and user input value (event property?)
-var psychicGuess = letters[Math.floor(Math.random() * letters.length)];
-var userGuess =event.key;
-var myGuess = userGuess; //why is it shadowing out my var names?
-
-//now build if/else loop
-
-//step 1 above 
-if (userGuess = letters[Math.floor(Math.random() * letters.length )]){
-
-if (userGuess === letters){
+    // If there is then we win and we'll update the HTML to display the win.
     wins++;
-}
-}
-//step 2
-else if (GuessLeft == 0){
+    document.querySelector("#wins").innerHTML = wins;
+
+    // Then we'll reset the game
+    reset();
+  }
+
+  // If we are out of guesses...
+  if (guessesLeft === 0) {
+
+    // Then we will loss and we'll update the HTML to display the loss.
     losses++;
-    GuessesLeft = 9;
-}
-//step 3
-else {
-    GuessesLeft --;
+    document.querySelector("#losses").innerHTML = losses;
 
-
-}
-}
-
-
-///Still Need To Do:
-// Figure out how to build reset into my four loop. ex: if (userGuess = letters){
-    // wins++ ()"}"
-    // else if (){
-    // guessesLeft = 0
-    // resestHTML (or something like that)
-    
-// figure out why the FUCK! my javascript wont link to my html
-//cry
-// 
+    // Then we'll call the reset.
+    reset();
+  }
+};
